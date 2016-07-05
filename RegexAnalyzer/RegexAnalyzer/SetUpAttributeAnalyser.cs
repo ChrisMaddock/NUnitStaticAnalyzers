010e43e2585b7eb18d4b61a566ffe869c862096b
Chris Maddock <chrismaddock@live.co.uk>
2016-07-05 20:00:49 +0100

    Initial commit

---
+++ RegexAnalyzer/RegexAnalyzer/SetUpAttributeAnalyser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace RegexAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SetUpAttributeAnalyzer : DiagnosticAnalyzer
+    {
+        private const string DiagnosticId = "SetUpAttributeAnalyzer";
+        private const string Title = "SetUpAttribute has errors";
+        private const string MessageFormat = "Method '{0}' with SetUpAttribute must be public";
+        private const string Description = "Methods with SetUpAttribute must be public";
+        private const string Category = "Naming";
+
+        private static string AttributeName = typeof(SetUpAttribute).Name;
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = context.Symbol;
+            var attributes = symbol.GetAttributes();
+
+            if (!attributes.Any(a => a.AttributeClass.Name == AttributeName)) return;
+            if (symbol.DeclaredAccessibility != Accessibility.Private) return;
+
+            var diagnostic = Diagnostic.Create(Rule, symbol.Locations[0], symbol.Name);
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
